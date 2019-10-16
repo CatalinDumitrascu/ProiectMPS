5011b2cecbecd7921e3a0ae89afb6ca66cc27477
Bianca Marilena Voinea <v.bmarilena@yahoo.com>
2019-10-16 09:24:05 +0300

    Create README.js

---
+++ README.js
@@ -0,0 +1,29 @@
+- install node js
+    1 https://nodejs.org/en/download/
+
+- install typescript
+    1 npm install -g typescript
+
+- install react native
+    1 https://facebook.github.io/react-native/docs/getting-started
+
+// PACKAGES
+
+- install react-native-navigation
+    1 npm install react-navigation
+    2 npm install react-native-gesture-handler
+    3 cd ios
+      pod install
+      cd ..
+
+- install createStackNavigator
+    1 npm install react-navigation-stack
+
+- install firebase
+    1 npm install firebase --save
+
+- for type errors
+    1 npm install @types/react
+    2 npm install @types/react-native
+
+- TODO install icons
