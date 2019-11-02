52059f74167478f31f65f33027e9aefa65ae95fe
DianaVlad <56344596+DianaVlad@users.noreply.github.com>
2019-11-02 12:00:21 +0200

    Update environment.ts

--- JuryDuty-server/src/environments/environment.ts
+++ JuryDuty-server/src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyDsFlAoQL7Ws4DOmZjA4diojyUBSq5X_0I",
-    authDomain: "juryduty-5b884.firebaseapp.com",
-    databaseURL: "https://juryduty-5b884.firebaseio.com",
-    projectId: "juryduty-5b884",
-    storageBucket: "juryduty-5b884.appspot.com",
-    messagingSenderId: "545827642477",
-    appId: "1:545827642477:web:8b3d358f67242ed765b364",
-    measurementId: "G-GX5WMEMBGH"
-  }
+  production: false
 };
 
 /*
